993c189e7430bcb247e083db21ca284c8f1b6dfa
Topskys <2477350343@qq.com>
2024-10-07 19:10:41 +0800

    feat: 新增SSE服务端事件推送接口

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Cache } from '@nestjs/cache-manager';
+import { interval, map, Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly cache: Cache,
-  ) {}
+  ) { }
 
   @Get()
   getHello() {
     return this.appService.getHello();
   }
+
+  @Sse('sse')
+  sse(): Observable<MessageEvent> {
+    return interval(1000).pipe(map((_) => {
+      return new MessageEvent('ping', {
+        data: { hello: 'sse' }
+      })
+    }));
+  }
+
 }
