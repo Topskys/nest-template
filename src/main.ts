be959e6ffe1311f981aa881126a5d999c1303dc2
Topskys <2477350343@qq.com>
2024-10-07 18:38:03 +0800

    feat: 新增swagger接口文档

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import helmet from 'helmet';
 import { Logger } from '@nestjs/common';
 import compression from 'compression';
 import { LoggerService } from './shared/logger/logger.service';
+import { swagger } from './utils/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await swagger(app)
   app.use(helmet());
   app.use(compression()); // gzip
   app.useLogger(app.get(LoggerService));
---
+++ src/utils/swagger.ts
@@ -0,0 +1,21 @@
+import { INestApplication } from "@nestjs/common"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
+
+/**
+ * Function to setup swagger documentation
+ * @param app Nest Application
+ * @returns Promise<unknown>
+ */
+export function swagger(app: INestApplication<any>) {
+    return new Promise((resolve, reject) => {
+        const options = new DocumentBuilder()
+            .setTitle("API Documentation")
+            .setDescription("API Documentation for the application")
+            .setVersion("1.0.0")
+            .addBearerAuth()
+            .build()
+        const document = SwaggerModule.createDocument(app, options)
+        SwaggerModule.setup("api", app, document)
+        resolve(app)
+    })
+}
